03a868f2fb034e41e27a73cfe331d61b9bb93051
Andrea Stagi <stagi.andrea@gmail.com>
2015-07-08 15:27:05 +0200

    Add new output format

--- nephilaionic.sh
+++ nephilaionic.sh
@@ -1,22 +1,25 @@
+echo -e "\x1B[1;97;44mCreating $1 project...\x1B[0m"
 ionic start $1 $2
 cd $1
-echo -e "\x1B[44mPreparing Android stuff...\x1B[0m"
-ionic platform add android
-echo -e "\x1B[44mDone.\x1B[0m"
+echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
-echo -e "\x1B[44mPreparing React.js stuff...\x1B[0m"
+echo -e "\x1B[1;97;44mPreparing Android stuff...\x1B[0m"
+ionic platform add android
+echo -e "\x1B[1;97;44mDone.\x1B[0m"
+echo ""
+echo -e "\x1B[1;97;44mPreparing React.js stuff...\x1B[0m"
 mkdir jsx
 npm install --save-dev gulp-react
-echo -e "\x1B[44mDone.\x1B[0m"
+echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
-echo -e "\x1B[44mPreparing custom styles...\x1B[0m"
+echo -e "\x1B[1;97;44mPreparing custom styles...\x1B[0m"
 touch scss/style.scss
-echo -e "\x1B[44mDone.\x1B[0m"
+echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
-echo -e "\x1B[44mPreparing gulpfile.js...\x1B[0m"
+echo -e "\x1B[1;97;44mPreparing gulpfile.js...\x1B[0m"
 wget -N https://raw.githubusercontent.com/nephila/ionic-template/master/gulpfile.js
-echo -e "\x1B[44mDone.\x1B[0m"
+echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
-echo -e "\x1B[44mPreparing libraries...\x1B[0m"
+echo -e "\x1B[1;97;44mPreparing libraries...\x1B[0m"
 bower install
-echo -e "\x1B[44mDone.\x1B[0m"
+echo -e "\x1B[1;97;44mDone.\x1B[0m"
