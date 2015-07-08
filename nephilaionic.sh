72b9d0ba10f8a7fa9114badda0547b98b0179d0b
Andrea Stagi <stagi.andrea@gmail.com>
2015-07-08 08:51:18 +0200

    Add nephilaionic.sh

---
+++ nephilaionic.sh
@@ -0,0 +1,18 @@
+#ionic start $1
+cd $1
+echo -e "\x1B[44mPreparing Android stuff...\x1B[0m"
+ionic platform add android
+echo -e "\x1B[44mDone.\x1B[0m"
+echo ""
+echo -e "\x1B[44mPreparing React.js stuff...\x1B[0m"
+mkdir jsx
+npm install --save-dev gulp-react
+echo -e "\x1B[44mDone.\x1B[0m"
+echo ""
+echo -e "\x1B[44mPreparing custom styles...\x1B[0m"
+touch scss/style.scss
+echo -e "\x1B[44mDone.\x1B[0m"
+echo ""
+echo -e "\x1B[44mPreparing gulpfile.js...\x1B[0m"
+wget
+echo -e "\x1B[44mDone.\x1B[0m"
