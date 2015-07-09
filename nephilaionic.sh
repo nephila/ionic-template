fa26345fe0a8b583597b6b4f037ea2883d25ab5d
Andrea Stagi <stagi.andrea@gmail.com>
2015-07-09 15:30:38 +0200

    Add commands to install linters

--- nephilaionic.sh
+++ nephilaionic.sh
@@ -17,6 +17,8 @@ touch scss/style.scss
 echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
 echo -e "\x1B[1;97;44mPreparing gulpfile.js...\x1B[0m"
+wget -N https://raw.githubusercontent.com/nephila/ionic-template/master/.jshintrc
+wget -N https://raw.githubusercontent.com/nephila/ionic-template/master/.scss-lint.yml
 wget -N https://raw.githubusercontent.com/nephila/ionic-template/master/gulpfile.js
 echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
@@ -24,6 +26,8 @@ echo -e "\x1B[1;97;44mPreparing libraries...\x1B[0m"
 bower install
 bower install --save react
 npm install
+npm install gulp-scss-lint --save-dev
+npm install gulp-jshint --save-dev
 echo -e "\x1B[1;97;44mDone.\x1B[0m"
 echo ""
 echo -e "\x1B[1;97;44mCompiling...\x1B[0m"
